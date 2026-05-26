2af211d7e626a19825c03ee26db1b79282962ece
SayyidSaalim <salimsalim17788@gmail.com>
2026-05-26 16:12:13 +0530

    Add uptime route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Salah Tracker Backend Running")),
+
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/prayers/', include('prayers.urls')),
