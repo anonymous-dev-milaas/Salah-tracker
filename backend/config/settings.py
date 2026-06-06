3e5963ea8d4fa52af1277def87ca3f8753a83994
SayyidSaalim <salimsalim17788@gmail.com>
2026-06-06 18:26:49 +0530

    Increase JWT lifetime

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,8 +92,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
